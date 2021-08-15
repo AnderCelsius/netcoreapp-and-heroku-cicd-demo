fe18b3eedf8c1b66f1ce4f95dca876403acf5c85
obinna asiegbulam <oasiegbulam@gmail.com>
2021-08-15 13:52:38 +0100

    Add project files.

---
+++ Calculations.Tests/CalculationsTest.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Calculations.Tests
+{
+    public class CalculationsTest
+    {
+        [Fact]
+        public void Add_GivenTwoIntegerNumbers_ReturnSum()
+        {
+            var calc = new Calculator();
+            int result = calc.Add(1, 2);
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void AddDouble_GivenTwoDoubleValues_ReturnSum()
+        {
+            var calc = new Calculator();
+            double result = calc.AddDouble(1.53, 1.45);
+            Assert.Equal(2.9, result, 0);
+        }
+
+        [Fact]
+        [Trait("Category", "Fibonacci")]
+        public void CheckFiboIsNotZero()
+        {
+            var calc = new Calculator();
+            //Assert.All(calc.FiboNumbers, num => Assert.NotEqual(0, num));
+            Assert.DoesNotContain(0, calc.FiboNumbers);
+        }
+
+        [Fact]
+        [Trait("Category", "Fibonacci")]
+        public void Check13Exists()
+        {
+            var calc = new Calculator();
+            Assert.Contains(13, calc.FiboNumbers);
+        }
+
+        [Fact]
+        [Trait("Category", "Fibonacci")]
+        public void CheckFiboNumbers()
+        {
+            var allFiboNumbers = new List<int> { 1, 1, 2, 3, 5, 8, 13 };
+            var calc = new Calculator();
+            Assert.Equal(allFiboNumbers, calc.FiboNumbers);
+        }
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,30 @@
+#See https://aka.ms/containerfastmode to understand how Visual Studio uses this Dockerfile to build your images for faster debugging.
+
+FROM mcr.microsoft.com/dotnet/sdk:5.0 AS base
+WORKDIR /src
+COPY *.sln .
+COPY Calculations/*.csproj Calculations/
+RUN dotnet restore Calculations/*.csproj
+COPY Calculations.Tests/*.csproj Calculations.Tests/
+RUN dotnet restore Calculations.Tests/*.csproj
+COPY . .
+
+#Testing
+FROM base AS testing
+WORKDIR /src/Calculations
+RUN dotnet build 
+WORKDIR /src/Calculations.Tests
+RUN dotnet test 
+
+#Publishing
+FROM build AS publish
+WORKDIR /src/Calculations
+RUN dotnet publish -c Release -o /src/publish
+
+
+#Get the run time into a folder called app
+FROM mcr.microsoft.com/dotnet/runtime:5.0 AS runtime
+WORKDIR /app
+COPY --from=publish /src/publish .
+#ENTRYPOINT ["dotnet", "Calculations.dll"]
+CMD ASPNETCORE_URLS=http://*:$PORT dotnet Calculations.dll
